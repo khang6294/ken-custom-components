6bc4b0d8628a076119f44727205e66ea0a9d762d
Khang Nguyen <ken.nguyen.bk62@gmail.com>
2018-12-16 06:08:52 +0700

    First custom component: Tooltip (without styling)

---
+++ tooltip.js
@@ -0,0 +1,27 @@
+class ToolTip extends HTMLElement {
+    constructor(){
+        super();
+        this._tooltipContainer;
+    }
+
+    connectedCallback(){
+        const tooltipIcon = document.createElement('span')
+        tooltipIcon.textContent = ' (?)'
+        tooltipIcon.addEventListener('mouseenter',this._showTooltip.bind(this));
+        tooltipIcon.addEventListener('mouseleave',this._hideTooltip.bind(this));
+        this.appendChild(tooltipIcon)
+    }
+
+    _showTooltip(){
+        this._tooltipContainer = document.createElement('div');
+        this._tooltipContainer.textContent = 'This is the tooltip text!';
+        this.appendChild(this._tooltipContainer);
+    }
+
+    _hideTooltip(){
+        this.removeChild(this._tooltipContainer);
+    }
+
+}
+
+customElements.define('kn-tooltip',ToolTip)
