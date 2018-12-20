1f330d6d7c6b2d816f124af472799549b6b131c5
Khang Nguyen <ken.nguyen.bk62@gmail.com>
2018-12-21 05:32:40 +0700

    Initalize STOCK PRICE component and test stock API

---
+++ ken-web-components-stencil-2/src/components/stock-price/stock-price.tsx
@@ -0,0 +1,33 @@
+import { Component, State } from "@stencil/core";
+
+@Component({
+    tag: 'kn-stock-price',
+    styleUrl: './stock-price.css',
+    shadow: true
+})
+
+export class StockPrice {
+    @State() fetchedPrice: number;
+    onFetchStockPrice =(event: Event) => {
+        event.preventDefault();
+        fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo')
+            .then(res => {
+                return res.json();
+            })
+            .then(parsedRes => {
+                this.fetchedPrice = +parsedRes['Global Quote']['05. price']
+            })
+            .catch(err => console.log(err))
+    }
+    render(){
+        return[
+            <form onSubmit={this.onFetchStockPrice}>
+                <input id='stock-symbol'/>
+                <button type='submit'>Fetch</button>
+            </form>,
+            <div>
+                <p>Price: ${this.fetchedPrice}</p>
+            </div>
+        ];
+    }
+}
